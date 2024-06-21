ef838400b8312175d47b6bd98dd2379c98b1c89e
RaymondLam18 <1024322@hr.nl>
2024-06-21 14:19:07 +0200

    Update MapComponent.js
    darkmode only for android

--- app/components/MapComponent.js
+++ app/components/MapComponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 const MapComponent = ({ region: initialRegion, location, places, onPressMarker, isDarkMode, fontSize }) => {
     const [region, setRegion] = useState(initialRegion); // State voor de kaartregio
@@ -28,10 +28,93 @@ const MapComponent = ({ region: initialRegion, location, places, onPressMarker, 
         }));
     };
 
+    // const darkMapStyle = [
+    //     { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+    //     { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+    //     { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+    //     {
+    //         featureType: 'administrative.locality',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#d59563' }]
+    //     },
+    //     {
+    //         featureType: 'poi',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#d59563' }]
+    //     },
+    //     {
+    //         featureType: 'poi.park',
+    //         elementType: 'geometry',
+    //         stylers: [{ color: '#263c3f' }]
+    //     },
+    //     {
+    //         featureType: 'poi.park',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#6b9a76' }]
+    //     },
+    //     {
+    //         featureType: 'road',
+    //         elementType: 'geometry',
+    //         stylers: [{ color: '#38414e' }]
+    //     },
+    //     {
+    //         featureType: 'road',
+    //         elementType: 'geometry.stroke',
+    //         stylers: [{ color: '#212a37' }]
+    //     },
+    //     {
+    //         featureType: 'road',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#9ca5b3' }]
+    //     },
+    //     {
+    //         featureType: 'road.highway',
+    //         elementType: 'geometry',
+    //         stylers: [{ color: '#746855' }]
+    //     },
+    //     {
+    //         featureType: 'road.highway',
+    //         elementType: 'geometry.stroke',
+    //         stylers: [{ color: '#1f2835' }]
+    //     },
+    //     {
+    //         featureType: 'road.highway',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#f3d19c' }]
+    //     },
+    //     {
+    //         featureType: 'transit',
+    //         elementType: 'geometry',
+    //         stylers: [{ color: '#2f3948' }]
+    //     },
+    //     {
+    //         featureType: 'transit.station',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#d59563' }]
+    //     },
+    //     {
+    //         featureType: 'water',
+    //         elementType: 'geometry',
+    //         stylers: [{ color: '#17263c' }]
+    //     },
+    //     {
+    //         featureType: 'water',
+    //         elementType: 'labels.text.fill',
+    //         stylers: [{ color: '#515c6d' }]
+    //     },
+    //     {
+    //         featureType: 'water',
+    //         elementType: 'labels.text.stroke',
+    //         stylers: [{ color: '#17263c' }]
+    //     }
+    // ];
+
     return (
         <View style={[styles.container, isDarkMode && styles.darkContainer]}>
             {region ? (
                 <MapView
+                    // provider={PROVIDER_GOOGLE}
+                    // customMapStyle={isDarkMode ? darkMapStyle : []}
                     style={styles.map}
                     region={region} // De huidige regio van de kaart
                     onRegionChangeComplete={(region) => setRegion(region)} // Regio bijwerken bij verandering
